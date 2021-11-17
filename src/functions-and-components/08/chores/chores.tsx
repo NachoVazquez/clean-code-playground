93cdbf63b1ea502de2587317bc6ecdde80086dbf
Nacho Vazquez <nachovc1410@gmail.com>
2021-11-17 20:30:46 -0300

    feat: extract use-toggle-action into a custom hook

---
+++ src/functions-and-components/08/chores/chores.tsx
@@ -0,0 +1,49 @@
+import ChoreItem from "../chore-item/chore-item";
+import useChoresState from "./use-chores-state";
+
+export default function Chores() {
+  const {
+    search,
+    completeChore,
+    uncompleteChore,
+    filteredUncompletedChoresByTitle,
+    filteredCompletedChoresByTitle,
+    filterTerm,
+  } = useChoresState();
+
+  return (
+    <div>
+      <h1>Chores</h1>
+      <input
+        type="search"
+        placeholder="Search"
+        value={filterTerm}
+        onChange={search}
+      />
+      <div>
+        <h2>Pending</h2>
+        <ul>
+          {filteredUncompletedChoresByTitle.map((chore) => (
+            <ChoreItem
+              key={chore.id}
+              chore={chore}
+              onChoreCompleted={completeChore}
+            />
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Completed</h2>
+        <ul>
+          {filteredCompletedChoresByTitle.map((chore) => (
+            <ChoreItem
+              key={chore.id}
+              chore={chore}
+              onChoreUncompleted={uncompleteChore}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
